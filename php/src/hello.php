a3e8429c8ce897bf35c8725b8a845c82e67bca66
ktogias <ktogias@fireball2.cti.gr>
2020-08-24 16:36:44 +0300

    compute code coverage

--- php/src/hello.php
+++ php/src/hello.php
@@ -4,7 +4,4 @@ echo json_encode('hello');
 if (false){
     dsfsdf;
     dfdsfd;
-    dfsfdsf;
-    dsffdsfds;
-    fsdfdf;
 }
--- util/test_code_coverage.sh
+++ util/test_code_coverage.sh
@@ -1,7 +1,9 @@
-RATIO=$(bash ./util/compute_code_coverage.sh ./php/tests/_output/coverage.xml)
+RATIO=$(bash ./util/compute_code_coverage.sh $1)
 echo $RATIO
 if [ $RATIO \< ".70" ]
 then
     echo "Code coverage below 70%"
     exit 1
 fi
+PREVIOUS_RATIO=$(bash ./util/compute_code_coverage.sh $2)
+echo $PREVIOUS_RATIO
